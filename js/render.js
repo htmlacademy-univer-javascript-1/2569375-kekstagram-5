86dcae0c18dfe8499ad6a6cdb59940b1692c7cb2
KosmosMF1 <mishafp.7@gmail.com>
2024-12-07 23:08:51 +0500

    Открывается и закрывается

--- js/render.js
+++ js/render.js
@@ -2,12 +2,15 @@ export const renderPictures = (pictures) => {
   const template = document.querySelector('#picture').content;
   const picturesContainer = document.querySelector('.pictures');
 
-  pictures.forEach(({ url, description, likes, comments }) => {
+  pictures.forEach((picture, index) => {
+    const { url, description, likes, comments } = picture;
     const pictureElement = template.cloneNode(true);
+
     pictureElement.querySelector('img').src = url;
     pictureElement.querySelector('img').alt = description;
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture').setAttribute('data-index', index);
     picturesContainer.appendChild(pictureElement);
   });
 };
