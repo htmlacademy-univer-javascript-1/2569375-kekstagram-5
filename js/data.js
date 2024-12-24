76aff7a1875105670a92f1ee4b013de429b53c2d
KosmosMF1 <mishafp.7@gmail.com>
2024-12-24 15:28:05 +0500

    Надо подкачаться

--- js/data.js
+++ js/data.js
@@ -1,27 +1,10 @@
-import { getRandomNumber, getRandomElement } from './util.js';
+import { getData } from './api.js';
 
-const commentsMessages = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота.',
-  'Лица у людей на фотке перекошены, как будто их избивают.',
-];
+const DATA_URL = 'https://29.javascript.htmlacademy.pro/kekstagram/data';
 
-const names = ['Намджун', 'Чонгук', 'Чингачгук', 'Гойко Митич', 'Дмин', 'Юнги', 'Джо Пич', 'Ясос Биб'];
-
-export const generateComments = (count) => {
-  const comments = [];
-
-  for (let i = 0; i < count; i++) {
-    comments.push({
-      id: i + 1,
-      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
-      message: getRandomElement(commentsMessages),
-      name: getRandomElement(names),
-    });
-  }
-
-  return comments;
+const getPhotos = async () => {
+  const photos = await getData(DATA_URL);
+  return photos;
 };
+
+export { getPhotos };
--- js/util.js
+++ js/util.js
@@ -1,3 +1,11 @@
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-export const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
+  return Math.floor(result);
+};
+
+const getRandomElement = (items) => items[getRandomInteger(0, items.length - 1)];
+
+export { getRandomElement, getRandomInteger };
